fea90783d0962cbc3265c92e8ec115cbf60ea87d
ylk19002090323 <2265943528@qq.com>
2020-03-16 09:32:46 +0800

    add b

---
+++ stat.js
@@ -0,0 +1,21 @@
+(function (){
+
+
+  function reduce(array,combine,start){
+    var current=start;
+    for(var element of array){
+        current=combine(current,element);
+    }
+    return current;
+}
+
+var max=function(data){
+  return reduce(
+      data,function(a,b) {
+  return a>b.credit?a:b.credit;
+  }
+  ,0)
+}
+
+window.my={"max":max};
+})()
