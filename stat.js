bc68b4d7c8992aa14dec2580390afbb54cc96ff1
ylk19002090323 <2265943528@qq.com>
2020-06-12 09:57:24 +0800

    add login

--- stat.js
+++ stat.js
@@ -1,21 +1,30 @@
-(function (){
+(function(){
 
 
-  function reduce(array,combine,start){
-    var current=start;
-    for(var element of array){
-        current=combine(current,element);
+    function reduce(array, combine, start) {
+        var current = start;//6
+        for (var element of array) {
+            current = combine(current, element);
+        }
+        return current;
+    }   
+    var max=function(data){
+        return reduce(
+            data, 
+            function(a,b){
+                return a>b.credit?a:b.credit;
+                }
+            , 0)
     }
-    return current;
-}
 
-var max=function(data){
-  return reduce(
-      data,function(a,b) {
-  return a>b.credit?a:b.credit;
-  }
-  ,0)
-}
+    window.my={"max":max};
 
-window.my={"max":max};
-})()
+}())
+
+
+   
+
+
+    
+    
+
